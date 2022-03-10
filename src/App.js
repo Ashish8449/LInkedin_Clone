30b6b47658424084792b24d35ec5b727b4aee55e
ashish8449 <ashishgururani8449@gmail.com>
2022-03-10 17:26:22 +0530

    completed login section

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { BrowserRouter } from "react-router-dom";
+import { Routes, Route, Link } from "react-router-dom";
+import Login from './Components/Login';
+import Header from './Components/Header';
+import Home from './Components/Home';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/home" element={<><Header /> <Home /> </>} />
+      </Routes>
+    </BrowserRouter>
+
+
   );
 }
 
